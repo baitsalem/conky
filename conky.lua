28d1d5a21b01695185d3285ee45fe0d347af2c90
baitsalem <baitsalem@gmail.com>
2020-07-07 22:44:37 +0200

    manage several lan interfaces

--- conky.lua
+++ conky.lua
@@ -1,11 +1,7 @@
 -- Find local IP address according to active connections
-function conky_myaddr()
-  return conky_parse('${addr ' .. findInterface() .. '}')
-end
 
 -- Find up/download speed
 function conky_myspeed(upordown)
-  print(findInterface())
   return conky_parse('${' .. upordown .. 'speed ' .. findInterface() .. '}')
 end
 
@@ -14,7 +10,67 @@ function conky_myspeedgraph(upordown, h, w)
   return conky_parse('${'..upordown..'speedgraph '..findInterface()..' '..h..','..w..'}')
 end
 
--- Find the string representing the connection's interface (e.g.  eth0)
+
+-- Wired interfaces
+function conky_myaddr()
+  return conky_parse('${addr ' .. findInterface() .. '}')
+end
+
+
+function conky_showints()
+
+
+	local interfaces = findInterface()
+	local result = ''
+
+	local lines = {}
+	for s in interfaces:gmatch("[^\r\n]+") do
+    		table.insert(lines, s)
+	end
+
+	 for _, interface in ipairs(lines) do
+		
+		result = result .. parse_interface(interface)
+	 end
+	 return conky_parse(result)
+end
+
+function parse_interface(interface)
+
+	return '${color7}Lan: ${color4}'.. interface .. ' ${color}${alignr}${color7}IP:${color2}${addr ' .. interface ..'}\n' .. '${color7}Download: ${color2}' .. '${downspeed ' .. interface .. '}k/s ${alignr}$alignr${color7}Upload: ${color3}' .. '${upspeed ' .. interface .. '}k/s\n' .. '${color lightgray}${downspeedgraph ' .. interface .. ' 30,120}${alignr}${upspeedgraph ' .. interface .. ' 30,120}$color\n'
+
+end
+
+
+function conky_showwints()
+
+
+	local interfaces = findWInterface()
+	local result = ''
+
+	local lines = {}
+	for s in interfaces:gmatch("[^\r\n]+") do
+    		table.insert(lines, s)
+	end
+
+	 for _, interface in ipairs(lines) do
+		
+		result = result .. parse_winterface(interface)
+	 end
+	 return conky_parse(result)
+end
+
+function parse_winterface(interface)
+
+	return '${color7}Wifi: ${color4}'.. interface .. ' ${color}${alignr}${color7}IP:${color2}${addr ' .. interface ..'}\n' .. '${color7}ESSID: ${color1}${wireless_essid ' .. interface  '}${color7} Rate:${color1}${wireless_bitrate ' .. interface .. '}\n' .. '${color7}Download: ${color2}' .. '${downspeed ' .. interface .. '}k/s ${alignr}$alignr${color7}Upload: ${color3}' .. '${upspeed ' .. interface .. '}k/s\n' .. '${color lightgray}${downspeedgraph ' .. interface .. ' 30,120}${alignr}${upspeedgraph ' .. interface .. ' 30,120}$color\n'
+
+end
+
+--${color7}ESSID:${color1}${lua myssid} ${color7}Rate:${color1}${lua wbitrate}
+--${color7}Quality:${color1}${lua wlinkqual}%${color}${lua wlinkbar 8,75}
+
+
+-- Find the string representing the connection's interface (e.g.  enp0s0)
 function findInterface()
   local handle = io.popen('ip a | grep "state UP" | cut -d: -f2 | tr -d " " | grep -E "en.*"')
   local result = handle:read('*a'):gsub('\n$','')
@@ -23,13 +79,15 @@ function findInterface()
 end
 
 
+
+
+-- wireless interfaces
 function conky_mywaddr()
   return conky_parse('${addr ' .. findWInterface() .. '}')
 end
 
 -- Find up/download speed
 function conky_mywspeed(upordown)
-  print(findInterface())
   return conky_parse('${' .. upordown .. 'speed ' .. findWInterface() .. '}')
 end
 
